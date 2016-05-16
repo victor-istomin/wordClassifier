f1d827819639df94f0969a77e29bc183a8bb86a2
Victor Istomin <victor.end.mail.here@gmail.com>
2016-05-16 08:52:10 -0700

    cat100 script

--- testcases/cat100.js
+++ testcases/cat100.js
@@ -1,1 +1,12 @@
-val = 
+var result = {};
+
+for (let i=0; i<100; ++i)
+{
+    let file = require('./'+i);
+    for (prop in file)
+    {
+        result[prop] = file[prop];
+    }
+}
+
+console.log(JSON.stringify(result));
